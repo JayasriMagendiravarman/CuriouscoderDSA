38a7859aba365ccae7539f06d4ec3119e8c31225
JayasriMagendiravarman <jayasrivarman15@gmail.com>
2025-06-24 13:10:19 +0530

    Add files via upload

---
+++ prob6.java
@@ -0,0 +1,9 @@
+class prob6{
+    public static void main(String[] args){
+        int i= 100;
+        for(int j=i; j<=1000; j=j+100)
+        {
+            System.out.println(j);
+        }
+    }
+}
