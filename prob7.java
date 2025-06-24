1f54c2341ecc2fdfae4dfc3167f000c6250df241
JayasriMagendiravarman <jayasrivarman15@gmail.com>
2025-06-24 13:10:40 +0530

    Add files via upload

---
+++ prob7.java
@@ -0,0 +1,11 @@
+class prob7{
+    public static void main(String[] args){
+        String firstname ="nandhini";
+        String lastname ="raja";
+        String n = firstname+lastname;
+        for(int i=1; i<=5; i=i+1)
+        {
+            System.out.println(n);
+        }
+    }
+}
